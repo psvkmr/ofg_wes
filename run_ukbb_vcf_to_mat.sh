d8a77105e3dd6b7a4c8dd8365d618f7db4fc1974
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-07-12 12:18:22 +0100

    ask for more resources

--- run_ukbb_vcf_to_mat.sh
+++ run_ukbb_vcf_to_mat.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #SBATCH --partition=brc,shared
 #SBATCH --job-name=vcfToMat
 #SBATCH --time=04:00:00
-#SBATCH --mem=32G
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --mem=48G
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=8
 #SBATCH --verbose
 #SBATCH --output=/scratch/users/k2142172/tests/vcfToMat.out
+
+conda activate r4
 
 Rscript /scratch/users/k2142172/scripts/ofg_wes/ukbb_vcf_to_mat.R
